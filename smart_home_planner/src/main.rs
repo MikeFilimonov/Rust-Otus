8e6375269aefa8f0b94f1d910ccc9eab65722036
Mike Filimonov <filimonov.michael@gmail.com>
2022-05-24 22:46:55 +0300

    another fix pack

--- smart_home_planner/src/main.rs
+++ smart_home_planner/src/main.rs
@@ -2,76 +2,85 @@ use std::collections::HashMap;
 
 struct SmartHome{
     
-    name : &str,
-    rooms: HashMap<&str, room>::new()
+    name : String,
+    rooms: HashMap<String, Room>
 }
 
 impl SmartHome {
     
-    fn get_room_list (&self) -> HashMap<&str, room>{
-        self.rooms
+    fn get_room_list (&self) -> &HashMap<String, Room>{
+        &self.rooms
     }
 
     fn add_room (&mut self, room_name: &str){
-        self.rooms.entry(room_name).or_insert(String::from(room_name), room::new(&room_name));   
+        
+        // self.rooms.entry(room_name).or_insert(String::from(room_name), Room::new(&room_name));  
+        self.rooms.insert(String::from(room_name), Room::new(&room_name)); 
     }
 
     fn remove_room (&mut self, room_name: &str){
         
-        match self.rooms.get(room_name){
+        /*match self.rooms.get(room_name){
             Some(value) => self.rooms.remove(&room_name),
             None => println!("The room named {} doesn't exist", &room_name)
-        }
+        }*/
+        self.rooms.remove(&room_name);
     }
 
 }
 
 struct Room {
     name: String,
-    smart_devices: HashMap<&str, smart_device>::new() 
+    smart_devices: HashMap<String, SmartDevice> 
 }
 
 impl Room {
     
     fn new (value: &str) -> Self {
-        
-        Room {
-            name,
-            smart_devices: HashMap<&str, smart_device>::new(),
-            }
+
+        Self.name,
+        smart_devices: HashMap<&str, SmartDevice>;
     }
 
-    fn get_device_list (&self){
-        self.smart_devices
+    fn get_device_list (&self) -> &HashMap<String, SmartDevice> {
+        &self.smart_devices
     }
     
     fn add_device (&mut self, id: &str){
-        self.smart_devices.entry(id).or_insert(String::from(id), smart_device::new(&id))       
+        // self.smart_devices.entry(id).or_insert(String::from(id), SmartDevice::new(&id))
+        self.smart_devices.insert(String::from(id), SmartDevice::new(&id));       
     }
 
     fn remove_device (&mut self, id: &str){
         
-        match self.smart_devices.get(id){
+        /*match self.smart_devices.get(id){
             Some(value) => self.smart_devices.remove(&id),
             None => println!("The device named {} doesn't exist", &id)
-        }
+        }*/
+        self.smart_devices.remove(&id);
     }
 
 }
 
-
-
-
-
 struct SmartDevice {
-    name: &str
-    
+    name: String
 }
 
 impl SmartDevice {
-    fn new(value: &str)->Self{
-        Self{value}
+    fn new(value: &str)-> Box<dyn Device>{
+       
+        /*self.name = value;
+        Self*/
+        Box::new(value)
+
     }
+}
+
+trait Device {
+    fn new (&self, name: String) -> Self{
+        Self{name}
+    }
+
 }
 
 fn main () {
