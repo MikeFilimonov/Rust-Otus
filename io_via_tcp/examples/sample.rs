aedc30d3cba4cc2eeb310e1310595d534ff9f69d
Mike Filimonov <filimonov.michael@gmail.com>
2022-07-12 00:56:55 +0300

    faulty response output

---
+++ io_via_tcp/examples/sample.rs
@@ -0,0 +1,52 @@
+use io_via_tcp::{
+    consts::{self, DEFAULT_OUTLET_ADDRESS},
+    Command, SmartOutletClient,
+};
+use std::io;
+
+fn main() {
+    let default_ip = DEFAULT_OUTLET_ADDRESS;
+    let mut client = SmartOutletClient::new(default_ip).unwrap();
+
+    loop {
+        show_disclaimer();
+
+        let user_input = handle_user_input();
+
+        let response = match user_input {
+            Some(command) => {client.execute(command).unwrap();},
+            None => {
+                println!("No user input detected...");
+                break;
+            }
+        };
+
+        // println!("Response: {}", response);
+        // print!(format!("Response:{response}", response));
+
+    }
+}
+
+fn show_disclaimer() {
+    let prompt = "Available commands:\n\
+    0: check consumption\n\
+    1: check state \n\
+    2: toggle outlet state \n\
+    _: locked feature";
+
+    print!("{}", prompt);
+}
+
+fn handle_user_input() -> Option<Command> {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    let command = match input.trim() {
+        "0" => Command::CheckConsumption,
+        "1" => Command::CheckState,
+        "2" => Command::ToggleState,
+        _ => return None,
+    };
+
+    Some(command)
+}
--- io_via_tcp/src/main.rs
+++ io_via_tcp/src/main.rs
@@ -1,3 +1,96 @@
+use io_via_tcp::{consts, Command, ServerResponse};
+use std::{
+    env,
+    io::{Read, Write},
+    net::TcpListener,
+};
+
+struct SmartOutletEmulator {
+    state: u32,
+    wattage: f32,
+}
+
+impl SmartOutletEmulator {
+    fn new() -> Self {
+        Self {
+            state: 0u32,
+            wattage: 0f32,
+        }
+    }
+
+    fn interact(&mut self, command: Command) -> ServerResponse {
+        match command {
+            Command::ToggleState => {
+                let new_state = match self.state {
+                    0u32 => 1u32,
+                    1u32 => 0u32,
+                    _ => 7u32,
+                };
+
+                self.wattage = match new_state {
+                    1u32 => 224.1f32,
+                    0u32 => 1.2f32,
+                    _ => -1.0f32,
+                };
+                ServerResponse::State(new_state)
+            }
+            Command::CheckConsumption => ServerResponse::Wattage(self.wattage),
+            Command::CheckState => {
+                let state = match self.state {
+                    1u32 => "on",
+                    0u32 => "off",
+                    _ => "broken",
+                };
+                let report = format!(" The device is {}, consumes {} W", state, self.wattage);
+                ServerResponse::Report(report)
+            }
+            Command::Explode => ServerResponse::TBD,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+    args.next().unwrap();
+
+    let server_address = args
+        .next()
+        .unwrap_or_else(|| consts::DEFAULT_OUTLET_ADDRESS.into());
+
+    let port_monitor =
+        TcpListener::bind(server_address).expect("Failed to bind to the given address");
+
+    let mut smart_outlet_emulator = SmartOutletEmulator::new();
+
+    while let Some(connection) = port_monitor.incoming().next() {
+        let mut stream = match connection {
+            Ok(result) => result,
+            Err(error) => {
+                println!("Can't establish connection due to error:{}", error);
+                continue;
+            }
+        };
+
+        let client = stream
+            .peer_addr()
+            .map(|address| address.to_string())
+            .unwrap_or_else(|_| "failed to get client ip".into());
+
+        println!("New client has connected, ip {}", client);
+
+        let mut data = [0u8];
+
+        while stream.read_exact(&mut data).is_ok() {
+            let response = smart_outlet_emulator.interact(data[0].into());
+            let data: [u8; 5] = response.into();
+            if stream.write_all(&data).is_err() {
+                break;
+            };
+        }
+
+        println!(
+            "Client {} has been disconnected. Ready for a new connection",
+            client
+        );
+    }
 }
