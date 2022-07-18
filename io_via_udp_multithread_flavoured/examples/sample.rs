30c197588cb9512c012336cc7d6f59ded78d1538
Mike Filimonov <filimonov.michael@gmail.com>
2022-07-18 22:05:07 +0300

    threads

--- io_via_udp_multithread_flavoured/examples/sample.rs
+++ io_via_udp_multithread_flavoured/examples/sample.rs
@@ -1,3 +1,15 @@
+use io_via_udp_multithread_flavoured::{consts, Thermometer};
+use std::{thread, time::Duration};
+
 fn main() {
-    println!("Hello, world")
+    
+    let receiver_address = consts::DEFAULT_THERMO_ADDRESS;
+    let thermometer = Thermometer::new(receiver_address).unwrap();
+
+    for _ in 0..consts::MAX_ITERAION_VALUE {
+        thread::sleep(Duration::from_secs(1));
+        let value = thermometer.get_data();
+        println!("Now it's {value} degrees C");
+    }
+
 }
--- io_via_udp_multithread_flavoured/src/main.rs
+++ io_via_udp_multithread_flavoured/src/main.rs
@@ -1,3 +1,47 @@
+use io_via_udp_multithread_flavoured::{consts, DataEmulator};
+use std::{
+    env,
+    net::{SocketAddr, UdpSocket},
+    thread,
+    time::Duration
+};
+
+
 fn main(){
-println!("Hey-hey-hey! We're working out")
+
+
+    let args = env::args();
+    let mut args = args.skip(1);
+    
+    let receiver_address = args
+        .next()
+        .unwrap_or_else(|| consts::DEFAULT_THERMO_ADDRESS.into());
+
+    println!("Receiver connected at {receiver_address}");
+    
+    let receiver = receiver_address
+        .parse::<SocketAddr>()
+        .expect("Invalid udp address. Please try another one.");
+    
+    let server_address = consts::DEFAULT_UDP_CLIENT;
+    let connection = UdpSocket::bind(server_address)
+        .expect("Can't bind to the address {server_address}");
+    let data_emulator = DataEmulator::default();
+
+
+    println!("Sending current temperature from {server_address} to {receiver_address} via UDP");
+
+    loop{
+        
+        let value = data_emulator.emulate();
+        let bytes = value.to_be_bytes();
+        let sent_data = connection.send_to(&bytes,receiver);
+        if let Err(err) = sent_data{
+            println!("Failed to share current temperature: {err}")
+        }
+        thread::sleep(Duration::from_secs(1))
+
+    }
+
+
 }
